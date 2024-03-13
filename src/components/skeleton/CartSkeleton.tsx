7234bca4b570589ab0315b561eaae35083f9ae40
Aashish Dhiman <aashishdhiman88@gmail.com>
2024-03-13 11:58:17 +0530

    fix: skeleton

--- src/components/skeleton/CartSkeleton.tsx
+++ src/components/skeleton/CartSkeleton.tsx
@@ -4,7 +4,7 @@ interface Props {}
 
 const CartSkeleton = (props: Props) => {
     return (
-        <div className="container mx-auto p-2 w-full">
+        <div className="container mx-auto w-full">
             <div className="flex flex-col md:flex-row gap-4 justify-between items-start w-full ">
                 <div className="flex-1 w-full md:w-[60%] flex flex-col gap-5">
                     <Skeleton className="w-full h-[70px] rounded-xl" />
@@ -12,7 +12,7 @@ const CartSkeleton = (props: Props) => {
                     <Skeleton className="w-full h-[70px] rounded-xl" />
                 </div>
                 {/* Order Total  */}
-                <div className="w-full md:w-[40%] px-2 mt-5">
+                <div className="w-full md:w-[40%] mt-5 sm:mt-0">
                     <Skeleton className="w-full h-[350px] rounded-lg" />
                 </div>
             </div>
