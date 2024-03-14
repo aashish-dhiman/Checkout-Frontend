83f5f0e9f38bb18d4ed91ca7855ca462d55b520d
Aashish Dhiman <aashishdhiman88@gmail.com>
2024-03-14 14:24:28 +0530

    final commit

--- src/components/navbar/Header.tsx
+++ src/components/navbar/Header.tsx
@@ -4,37 +4,16 @@ import { ModeToggle } from "../ToggleMode";
 import { useMerchantData } from "@/hooks/useMerchantData";
 import HeaderSkeleton from "../skeleton/HeaderSkeleton";
 import Image from "next/image";
-import { useDispatch } from "react-redux";
-import { setTheme } from "@/redux/features/theme-slice";
-import { AppDispatch } from "@/redux/store";
-import { GetThemeFromStore } from "@/lib/getStoreTheme";
 
 interface Props {}
 
 const Header = (props: Props) => {
     const { data, isError, isLoading, error } = useMerchantData();
-    // const dispatch = useDispatch<AppDispatch>();
-    // dispatch(setTheme(data?.theme));
-
-    // const theme = GetThemeFromStore();
 
     if (isLoading) return <HeaderSkeleton />;
-    // const themeColor = localStorage.getItem("theme") || "light";
 
     return (
-        <div
-            style={{
-                // backgroundColor:
-                //     themeColor === "dark" ? theme["--background"] : undefined,
-                // color:
-                //     themeColor === "dark"
-                //         ? theme["--primary-foreground"]
-                //         : undefined,
-                backdropFilter: "blur(8px)",
-                WebkitBackdropFilter: "blur(8px)",
-            }}
-            className="fixed top-0 w-full z-50 flex border-b-2 items-center justify-between px-3 sm:px-7 py-4 lg:px-[100px]"
-        >
+        <div className="fixed top-0 backdrop-blur w-full z-50 flex border-b-2 items-center justify-between px-3 sm:px-7 py-4 lg:px-[100px]">
             <div className="flex items-center gap-2 ">
                 <Image
                     src={data?.merchantLogo || "/logo.png"}
@@ -43,17 +22,7 @@ const Header = (props: Props) => {
                     height={60}
                     className="w-10 h-10 "
                 />
-                <h1
-                    style={{
-                        // color:
-                        //     themeColor === "dark"
-                        //         ? theme["--primary-foreground"]
-                        //         : undefined,
-                    }}
-                    className="text-lg font-bold"
-                >
-                    {data?.merchantName}
-                </h1>
+                <h1 className="text-lg font-bold">{data?.merchantName}</h1>
             </div>
             <ModeToggle />
         </div>
